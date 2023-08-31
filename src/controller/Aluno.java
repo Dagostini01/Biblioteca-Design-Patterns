5bd5d73bde018285795de3c8b14e3d95603f02e8
Pedro Dagostini <87324617+Dagostini01@users.noreply.github.com>
2023-08-31 14:29:23 -0300

    classe aluno feita

--- src/controller/Aluno.java
+++ src/controller/Aluno.java
@@ -1,4 +1,30 @@
 package controller;
 
 public class Aluno {
+    private String nome;
+    private int numeroMatricula;
+
+    public Aluno() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNumeroMatricula(int numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public int getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public Aluno(String nome, int numeroMatricula) {
+        this.nome = nome;
+        this.numeroMatricula = numeroMatricula;
+    }
 }
